ef0bcb3f92b744968e3497608d843f7586b563d7
RahwaZeslusHaile <rahwazeslase@gmail.com>
2025-08-21 15:45:12 +0100

    cleaning

--- server/server.js
+++ server/server.js
@@ -6,4 +6,4 @@ import { firebaseConfig ,auth} from "./services/firebaseConfig.js";
 
 console.log(process.env.FIREBASE_API_KEY);
 console.log(firebaseConfig);
-console.log("API Key:", process.env.FIREBASE_API_KEY); // Should print the key
+console.log("API Key:", process.env.FIREBASE_API_KEY); 
