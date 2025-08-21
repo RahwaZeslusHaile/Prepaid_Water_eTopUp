30de13c63a60175b554d9d4660c5c5cb47e29ba4
RahwaZeslusHaile <rahwazeslase@gmail.com>
2025-08-21 15:43:41 +0100

    Move Firebase keys to .env

---
+++ server/server.js
@@ -0,0 +1,9 @@
+
+import dotenv from "dotenv";
+dotenv.config();
+
+import { firebaseConfig ,auth} from "./services/firebaseConfig.js";
+
+console.log(process.env.FIREBASE_API_KEY);
+console.log(firebaseConfig);
+console.log("API Key:", process.env.FIREBASE_API_KEY); // Should print the key
